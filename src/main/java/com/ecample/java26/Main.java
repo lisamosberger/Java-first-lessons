7f7b7ffd8d69f1f46c26246e43d7afe0fd6295ab
Lisa <lisamosberger096@gmail.com>
2026-08-30 17:16:46 +0200

    Första Lektion

--- src/main/java/com/ecample/java26/Main.java
+++ src/main/java/com/ecample/java26/Main.java
@@ -3,7 +3,13 @@ package com.ecample.java26;
 public class Main {
     public static void main(String[] args) {
             IO.println("Hello World!");
-            int cadence = 3*5;
-            IO.println(cadence);
-        }
+            String name = "Max";
+            int age = 21; //whole numbers
+            double price = 10.99; //decimal numbers
+            boolean isOnline = true; //true/false
+            char grade = 'A'; //single characters
+            String firstname = "Max"; //text
+
+        IO.println("My name is " + name + " I am " + age + " years old!");
+    }
 }
