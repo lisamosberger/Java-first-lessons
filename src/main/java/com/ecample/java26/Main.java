c9b4aa3fc7f3ad00c985c95e5514ab1eef5e26cb
Lisa <lisamosberger096@gmail.com>
2026-08-30 17:15:25 +0200

    Första Lektion

--- src/main/java/com/ecample/java26/Main.java
+++ src/main/java/com/ecample/java26/Main.java
@@ -3,5 +3,7 @@ package com.ecample.java26;
 public class Main {
     public static void main(String[] args) {
             IO.println("Hello World!");
+            int cadence = 3*5;
+            IO.println(cadence);
         }
 }
