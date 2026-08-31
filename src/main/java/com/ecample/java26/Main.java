8e635330a2737b9b158223da95eba96ae27c4f1f
Lisa <lisamosberger096@gmail.com>
2026-08-31 11:46:52 +0200

    Första Lektion

--- src/main/java/com/ecample/java26/Main.java
+++ src/main/java/com/ecample/java26/Main.java
@@ -1,13 +1,18 @@
 package com.ecample.java26;
 
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
+            Locale.setDefault(Locale.ENGLISH);
             IO.println("Hello World!");
 
             int whole_numbers = 17;
             double decimal_numbers = 4.99;
             boolean true_false = true;
             char single_character = 'A';
+            var weight = 75.0;
+
             String text = "Hello World!";
             IO.println(text);
             IO.println(whole_numbers);
@@ -16,7 +21,7 @@ public class Main {
             IO.println(single_character);
 
             double length = 1.85;
-            System.out.printf("%.20f", length);
+            System.out.printf(Locale.ENGLISH, "%.20f", length);
 
             test_1();
         }
