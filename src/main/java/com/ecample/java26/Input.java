8959152360b0c4e786bdc7cf1bce193079027ef7
Lisa <lisamosberger096@gmail.com>
2026-08-31 14:40:59 +0200

    Första Lektion

--- src/main/java/com/ecample/java26/Input.java
+++ src/main/java/com/ecample/java26/Input.java
@@ -4,8 +4,14 @@ public class Input {
     static void main(){
 
         IO.println("Vad heter du?");
-        String input = IO.readln();
-        IO.println("Hej " + input);
-
+        String name = IO.readln();
+        IO.println("Hej " + name);
+        IO.println("Hur många katter har du?");
+        int catCount = Integer.parseInt(IO.readln() );
+        if (catCount == 1){
+            IO.println("Du har en katt");}
+        else {
+            IO.println("Du har " + catCount + " Katter");
+            IO.println("Om du skaffar en till har du " + ++catCount + " Katter");}
     }
 }
